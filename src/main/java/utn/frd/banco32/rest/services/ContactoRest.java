c095af43587aac7f250dfd1b8a000fdd283c6a29
zetta <zetta@Samsung>
2018-04-14 12:13:42 -0300

    commit inicial

---
+++ src/main/java/utn/frd/banco32/rest/services/ContactoRest.java
@@ -0,0 +1,57 @@
+package utn.frd.banco32.rest.services;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import utn.frd.banco32.entity.Contacto;
+import utn.frd.banco32.sessions.ContactoFacade;
+/**
+*
+* @author Sergio
+*/
+@Path("/Contacto")
+public class ContactoRest {
+@EJB
+private ContactoFacade ejbContactoFacade;
+//obtener todas las entidades
+@GET
+@Produces({MediaType.APPLICATION_JSON})
+public List<Contacto> findAll(){
+return ejbContactoFacade.findAll();
+}
+//crear entidades
+@POST
+@Consumes({MediaType.APPLICATION_JSON})
+public void create(Contacto contacto){
+ejbContactoFacade.create(contacto);
+}
+//actualizar entidades
+@PUT
+@Consumes({MediaType.APPLICATION_JSON})
+@Path("/{id}")
+public void edit(@PathParam("id")long id, Contacto contacto){
+ejbContactoFacade.edit(contacto);
+}
+//eliminar entidades
+@DELETE
+@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+@Path("/{id}")
+public void remove(@PathParam("id")long id){
+ejbContactoFacade.remove( ejbContactoFacade.find(id) );
+}
+//obtener una entidad por id
+@GET
+@Path("/{id}")
+@Produces({MediaType.APPLICATION_JSON})
+
+public Contacto findById(@PathParam("id")long id){
+    return ejbContactoFacade.find(id);
+}
+}
---
+++ src/main/java/utn/frd/banco32/sessions/ContactoFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utn.frd.banco32.sessions;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import utn.frd.banco32.entity.Contacto;
+
+/**
+ *
+ * @author zetta
+ */
+@Stateless
+public class ContactoFacade extends AbstractFacade<Contacto> {
+
+    @PersistenceContext(unitName = "utn.frd_banco32_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ContactoFacade() {
+        super(Contacto.class);
+    }
+    
+}
